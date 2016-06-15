c1d1acd9fef8e714f11475fb2f472c7e28508e16
Yura Panayotov <panayotov.yura@gmail.com>
2016-06-15 23:36:51 +0300

    add update comment action

--- src/BookmarksBundle/Controller/RESTController.php
+++ src/BookmarksBundle/Controller/RESTController.php
@@ -6,11 +6,13 @@ use BookmarksBundle\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use BookmarksBundle\Entity\Bookmark;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class RESTController extends Controller
@@ -27,44 +29,6 @@ class RESTController extends Controller
         $data = $this->getDoctrine()->getRepository(Bookmark::class)->findBy([], ['createdAt' => 'DESC'], 10);
 
         return $this->json($data);
-    }
-
-    /**
-     * @Route("/bookmark/{uid}/comment")
-     * @Method({"POST"})
-     *
-     * @param Bookmark $bookmark
-     * @param Request $request
-     *
-     * @throws BadRequestHttpException
-     *
-     * @return JsonResponse
-     */
-    public function createComment(Bookmark $bookmark, Request $request)
-    {
-        $text = $request->get('text');
-        $violationsList = $this->get('validator')->validate(
-            $text,
-            [
-                new Assert\NotBlank(),
-            ]
-        );
-
-        if (count($violationsList) > 0) {
-            throw new BadRequestHttpException($violationsList->get(0)->getMessage());
-        }
-
-        $comment = (new Comment())
-            ->setIp($request->getClientIp())
-            ->setText($text)
-            ->setBookmark($bookmark);
-
-        // todo: move to service
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($comment);
-        $entityManager->flush();
-
-        return $this->json(['uid' => $comment->getUid()], Response::HTTP_CREATED);
     }
 
     /**
@@ -119,5 +83,80 @@ class RESTController extends Controller
         }
 
         return $this->json(['uid' => $bookmark->getUid()], $responseStatus);
+    }
+
+    /**
+     * @Route("/bookmark/{uid}/comment")
+     * @Method({"POST"})
+     *
+     * @param Bookmark $bookmark
+     * @param Request $request
+     *
+     * @throws BadRequestHttpException
+     *
+     * @return JsonResponse
+     */
+    public function createComment(Bookmark $bookmark, Request $request)
+    {
+        $text = $request->get('text');
+        // todo: move to function
+        $violationsList = $this->get('validator')->validate(
+            $text,
+            [
+                new Assert\NotBlank(),
+            ]
+        );
+
+        if (count($violationsList) > 0) {
+            throw new BadRequestHttpException($violationsList->get(0)->getMessage());
+        }
+
+        $comment = (new Comment())
+            ->setIp($request->getClientIp())
+            ->setText($text)
+            ->setBookmark($bookmark);
+
+        // todo: move to service
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($comment);
+        $entityManager->flush();
+
+        return $this->json(['uid' => $comment->getUid()], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/comment/{uid}")
+     * @Method({"PUT"})
+     */
+    public function updateCommentAction(Comment $comment, Request $request)
+    {
+        if ($comment->getIp() != $request->getClientIp()) {
+            throw new NotFoundHttpException();
+        }
+
+        $putStr = $request->getContent();
+        parse_str($putStr, $putData);
+        $text = isset($putData['text']) ? $putData['text'] : null;
+        // todo: move to function
+        $violationsList = $this->get('validator')->validate(
+            $text,
+            [
+                new Assert\NotBlank(),
+            ]
+        );
+
+        if (count($violationsList) > 0) {
+            throw new BadRequestHttpException($violationsList->get(0)->getMessage());
+        }
+
+        // todo: move to service
+        if ($comment->getCreatedAt()->add(new \DateInterval('PT1H')) > new \DateTime()) {
+            $comment->setText($text);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+        }
+
+        return $this->json(['uid' => $comment->getUid()]);
+
     }
 }
